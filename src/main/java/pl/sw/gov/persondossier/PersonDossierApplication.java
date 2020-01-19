1b3999242cb7a16532452cc23569b45db7b0008e
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2020-01-19 16:31:00 +0100

    init commit 2

---
+++ src/main/java/pl/sw/gov/persondossier/PersonDossierApplication.java
@@ -0,0 +1,13 @@
+package pl.sw.gov.persondossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonDossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonDossierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sw/gov/persondossier/PersonDossierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sw.gov.persondossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonDossierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
